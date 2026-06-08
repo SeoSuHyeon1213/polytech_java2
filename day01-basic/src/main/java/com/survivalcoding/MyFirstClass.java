1a22451f736562dab87eccbb0193a7ec2aac209b
SuHyun <mneo1213@naver.com>
2026-06-08 10:30:53 +0900

    First

---
+++ day01-basic/src/main/java/com/survivalcoding/MyFirstClass.java
@@ -0,0 +1,9 @@
+package com.survivalcoding;
+
+public class MyFirstClass {
+    public static void main(String[] args){
+        System.out.println("Hello!!!");
+        int i = 1;
+        i = 19;
+    }
+}
