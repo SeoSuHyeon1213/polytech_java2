b704508c98a155e0879ebc2565c3bf21a36d1024
SuHyun <mneo1213@naver.com>
2026-06-08 10:33:40 +0900

    String 연습

--- day01-basic/src/main/java/com/survivalcoding/MyFirstClass.java
+++ day01-basic/src/main/java/com/survivalcoding/MyFirstClass.java
@@ -2,7 +2,9 @@ package com.survivalcoding;
 
 public class MyFirstClass {
     public static void main(String[] args){
+        String name = "ghghg";
         System.out.println("Hello!!!");
+        System.out.println("Hello!!!" + name);
         int i = 1;
         i = 19;
     }
