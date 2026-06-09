e356dc22a7e1a4d1684317b5283cab77765512e4
SuHyun <mneo1213@naver.com>
2026-06-09 15:59:09 +0900

    260609

--- game/src/test/java/com/survivalcoding/ClericTest.java
+++ game/src/test/java/com/survivalcoding/ClericTest.java
@@ -3,22 +3,28 @@ package com.survivalcoding;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class ClericTest {
 
     @Test
     @DisplayName("mp -5 & hp max heal")
     void add() {
         //given
-        Cleric cleric = new Cleric();
+        Cleric cleric = new Cleric("아서스", 40, 5);
+        Cleric cleric2 = new Cleric("아서스", 35);
+        Cleric cleric3 = new Cleric("아서스");
+        //Cleric cleric4 = new Cleric();
+
+
         //when
-        System.out.println(cleric.name); //Cleric
-        cleric.selfAid(); //hp max
-        cleric.pray(3); //5~8
-        cleric.pray(5); //10
+        System.out.println(cleric2.name);
+        System.out.println(cleric2.hp);
+        System.out.println(cleric2.mp);
+
+        System.out.println(cleric3.name);
+        System.out.println(cleric3.hp);
+        System.out.println(cleric3.mp);
 
         //then
-        assertEquals(10, cleric.mp); //10
+
     }
 }
